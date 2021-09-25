dc56de3899915f75eba6cf8c33ae43fec10aabc7
divya_vadher <divya.vadher@innvonix.com>
2021-09-25 19:09:59 +0530

    all pages

--- js/custom.js
+++ js/custom.js
@@ -87,6 +87,19 @@ $(document).ready(function () {
         }
     });
 
+    $('.show-more-content').hide();
+
+    $('.show-more').click(function(){
+        $('.show-more-content').show(300);
+        $('.show-less').show();
+        $('.show-more').hide();
+    });
+
+    $('.show-less').click(function(){
+        $('.show-more-content').hide(300);
+        $('.show-more').show();
+        $(this).hide();
+    });
 
     $('#qty_input').prop('disabled', true);
     $('#plus-btn').click(function(){
